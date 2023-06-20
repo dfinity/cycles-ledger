a94e31db8ebac2e1ef06bca7d973a96596645ccd
Severin Siffert <severin.siffert@dfinity.org>
2023-06-20 13:43:52 +0200

    add rejected inputs test

--- cycles-ledger/tests/tests.rs
+++ cycles-ledger/tests/tests.rs
@@ -137,14 +137,13 @@ fn test_send_fails() {
     assert_eq!(deposit_res.balance, Nat::from(1_000_000_000 - FEE));
 
     // send cycles to non-existent send_receiver
-    let send_amount = 500000000_u128;
     let send_result = send(env, ledger_id, user, SendArg{
         from_subaccount: None,
         to: send_receiver.into(),
         fee: None,
         created_at_time: None,
         memo: None,
-        amount: Nat::from(send_amount),
+        amount: Nat::from(500_000_000_u128),
     }).unwrap_err();
 
     assert!(matches!(send_result, cycles_ledger::endpoints::SendError::FailedToSend { rejection_code: RejectionCode::DestinationInvalid, .. }));
@@ -154,4 +153,57 @@ fn test_send_fails() {
         balance_of(env, ledger_id, user),
         Nat::from(1_000_000_000 - FEE - FEE)
     );
+}
+
+#[test]
+fn test_send_input_rejected() {
+    let env = &StateMachine::new();
+    let ledger_id = install_ledger(env);
+    let depositer_id = install_depositer(env, ledger_id);
+    let user = Account {
+        owner: PrincipalId::new_user_test_id(1).into(),
+        subaccount: None,
+    };
+
+    // make the first deposit to the user and check the result
+    let deposit_res = deposit(env, depositer_id, user, 1_000_000_000);
+    assert_eq!(deposit_res.txid, Nat::from(0));
+    assert_eq!(deposit_res.balance, Nat::from(1_000_000_000 - FEE));
+
+    // send more than available
+    let send_amount = 999_000_000_000_u128;
+    let send_result = send(env, ledger_id, user, SendArg{
+        from_subaccount: None,
+        to: depositer_id.into(),
+        fee: None,
+        created_at_time: None,
+        memo: None,
+        amount: Nat::from(send_amount),
+    }).unwrap_err();
+    println!("send more than available result: {:?}", &send_result);
+    assert!(matches!(send_result, cycles_ledger::endpoints::SendError::InsufficientFunds{ .. }));
+
+    // send from empty subaccount
+    let send_result = send(env, ledger_id, user, SendArg{
+        from_subaccount: Some([5; 32]),
+        to: depositer_id.into(),
+        fee: None,
+        created_at_time: None,
+        memo: None,
+        amount: Nat::from(100_000_000_u128),
+    }).unwrap_err();
+    println!("send from empty subaccount result: {:?}", &send_result);
+    assert!(matches!(send_result, cycles_ledger::endpoints::SendError::InsufficientFunds{ .. }));
+
+    // bad fee
+    let send_result = send(env, ledger_id, user, SendArg{
+        from_subaccount: None,
+        to: depositer_id.into(),
+        fee: Some(Nat::from(4)),
+        created_at_time: None,
+        memo: None,
+        amount: Nat::from(100_000_000_u128),
+    }).unwrap_err();
+    println!("bad fee result: {:?}", &send_result);
+    assert!(matches!(send_result, cycles_ledger::endpoints::SendError::BadFee{ .. }));
 }
