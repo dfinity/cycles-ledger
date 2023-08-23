704f3c0e3313cf3ce6b7a206dd5ac8e15725c284
NikolasHai <113891786+NikolasHai@users.noreply.github.com>
2023-08-23 13:13:42 +0200

    Update cycles-ledger/src/storage.rs
    Co-authored-by: Roman Kashitsyn <roman.kashitsyn@gmail.com>

--- cycles-ledger/src/storage.rs
+++ cycles-ledger/src/storage.rs
@@ -372,7 +372,7 @@ pub fn transfer(
             },
             timestamp: now,
             phash,
-            effective_fee: suggested_fee.map_or(Some(config::FEE), |_| None),
+            effective_fee: suggested_fee.is_none().then_some(config::FEE),
         });
         (s.blocks.len() - 1, block_hash)
     })
