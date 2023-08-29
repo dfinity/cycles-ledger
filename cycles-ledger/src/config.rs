ec6d891fe3daf2d3f036a58a5bc11d24a0494d6c
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2023-08-29 12:41:37 +0000

    const comment

--- cycles-ledger/src/config.rs
+++ cycles-ledger/src/config.rs
@@ -7,5 +7,7 @@ pub const TOKEN_SYMBOL: &str = "CYC";
 pub const MAX_MEMO_LENGTH: u32 = 32;
 pub const PERMITTED_DRIFT: Duration = Duration::from_secs(60);
 pub const TRANSACTION_WINDOW: Duration = Duration::from_secs(24 * 60 * 60);
+// The number of transactions in the transaction to hash and block timestamp to block index mappings to be pruned in a single prune process
 pub const MAX_TRANSACTIONS_TO_PURGE: usize = 100_000;
+// The number of entries in the approval list and expiration queue to be pruned in a single prune process
 pub const APPROVE_PRUNE_LIMIT: usize = 100;
